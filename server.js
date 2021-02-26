a1762d0308109562fd9b99ba496c9b1f53e5b028
Vicky Loebel <vickyloebel@gmail.com>
2021-02-25 19:56:11 -0700

    server

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/notes.html'));
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 
