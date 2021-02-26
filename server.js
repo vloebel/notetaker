4ce5ab793c99dfa41b6098e5471c3f3421c5fc1b
Vicky Loebel <vickyloebel@gmail.com>
2021-02-25 20:24:27 -0700

    index path

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
-const { notesDb } = require('./db/notesDb');
+const { notesDb } = require('./db/db');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -12,7 +12,7 @@ app.use(express.json());
 
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 
