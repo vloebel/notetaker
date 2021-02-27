54a96969a8570e5fc63a7d1c16413fc4edb8348d
Vicky Loebel <vickyloebel@gmail.com>
2021-02-27 14:01:54 -0700

    fixed bug in distribution files that set the note text to the title

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,13 +1,13 @@
 const router = require('express').Router();
-const { createNewNote, validateNote } = require('../../lib/notes');
-const notesArr = require('../../db/db.json');
+const { createNewNote, filterByQuery, validateNote } = require('../../lib/notes');
+const notesDatabase = require('../../db/db.json');
 
 router.get('/notes', (req, res) => {
-  let results = notesArr;
-  console.log({results});
-  // if (req.query) {
-  // //   results = filterByQuery(req.query, results);
-  // }
+  let results = notesDatabase;
+  console.log("GET NOTES - query is " + JSON.stringify(req.query));
+  if (req.query) {
+    results = filterByQuery(req.query, results);
+  }
   res.json(results);
 });
 
@@ -16,9 +16,7 @@ router.post('/notes', (req, res) => {
   if (!validateNote(req.body)) {
     res.status(400).send('This note is not properly formatted.');
   } else {
-    console.log("router.post: req.body is" + req.body);
-    console.log ( {notesArr} );
-    const note = createNewNote(req.body, notesArr);
+    const note = createNewNote(req.body, notesDatabase);
     res.json(note);
   }
 });
